eed2c39d67a390bf0a6deed3018534a6e3e00f21
Vincent Chan <vincent@vincentcchan.com>
2014-07-03 22:31:25 -0700

    update

--- app.js
+++ app.js
@@ -6,15 +6,32 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.render('index.ejs', {
-    bodyId: 'index',
-   });
+  res.redirect(301, '/jukebox')
 });
 
 app.get('/jukebox', function(req, res){
+
+  var ua = req.headers['user-agent'],
+      $ = {};
+
+  var display = 'Mac OSX 10.7+'
+
+  if (/mobile/i.test(ua))
+      $.Mobile = true;
+
+  if (/like Mac OS X/.test(ua)) {}
+      display = 'Mac OSX 10.7+'
+
+  if (/(Intel|PPC) Mac OS X/.test(ua))
+      display = 'Mac OSX 10.7+'
+
+  if (/Windows NT/.test(ua))
+      display = 'Windows XP+'
+
   res.render('jukebox.ejs', {
     bodyId: 'jukebox',
-    navActive: 'jukebox'
+    navActive: 'jukebox',
+    platform: display
   });
 })
 
